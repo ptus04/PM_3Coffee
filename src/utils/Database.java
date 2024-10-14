15f6d0a8ce8e1ffefe42b45777ad96a3c483f5ec
Phùng Văn Tú <tuphung.03032004@gmail.com>
2024-10-14 11:17:55 +0700

    feat: add database manager
    Signed-off-by: Phùng Văn Tú <tuphung.03032004@gmail.com>

---
+++ src/utils/Database.java
@@ -0,0 +1,41 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+
+	private final String url = "jdbc:sqlserver://localhost:1433;databaseName=3Coffee";
+	private final String user = "sa";
+	private String password;
+
+	public Connection connection;
+	public static Database connectDB;
+
+	public static Database getInstance() {
+		if (connectDB == null) {
+			connectDB = new Database();
+		}
+
+		return connectDB;
+	}
+
+	public Database() {
+		password = System.getenv("MSSQL_PASSWORD");
+	}
+
+	public Connection getConnection() throws SQLException {
+		if (connection == null || connection.isClosed()) {
+			connection = DriverManager.getConnection(url, user, password);
+		}
+
+		return connection;
+	}
+
+	public void disconnect() throws SQLException {
+		if (connection != null) {
+			connection.close();
+		}
+	}
+}
