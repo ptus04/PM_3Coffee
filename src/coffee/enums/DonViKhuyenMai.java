09e27524a569c20275e86298db6eb45ac7b8c878
Phùng Văn Tú <tuphung.03032004@gmail.com>
2024-10-14 18:38:07 +0700

    refactor: tổ chức lại thư mục và entity
    Signed-off-by: Phùng Văn Tú <tuphung.03032004@gmail.com>

--- src/coffee/enums/DonViKhuyenMai.java
+++
@@ -1,5 +0,0 @@
-package coffee.enums;
-
-public enum DonViKhuyenMai {
-	PHAN_TRAM, TRI_GIA_TRUC_TIEP
-}
--- src/coffee/enums/Quyen.java
+++
@@ -1,5 +0,0 @@
-package coffee.enums;
-
-public enum Quyen {
-	NHAN_VIEN_QUAN_LY, NHAN_VIEN_BAN_HANG
-}
