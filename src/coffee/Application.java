ed2df4ad318de374c8d95382444550fc6829a6d3
Phùng Văn Tú <tuphung.03032004@gmail.com>
2024-11-19 01:42:35 +0700

    Thêm các định dạng và thay đổi màu focus button vào Application
    Signed-off-by: Phùng Văn Tú <tuphung.03032004@gmail.com>

--- src/coffee/Application.java
+++ src/coffee/Application.java
@@ -1,22 +1,29 @@
 package coffee;
 
-import java.awt.Color;
+import java.text.NumberFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
+import coffee.constant.Colors;
 import coffee.controller.DangNhapController;
 import coffee.controller.TrangChuController;
 import coffee.entity.NhanVien;
 import coffee.entity.TaiKhoan;
-import coffee.entity.TrangThaiLamViec;
 import coffee.shared.utility.ColorUtilities;
 
 public class Application {
 
+	public static final DateTimeFormatter FMT_DATE = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	public static final DateTimeFormatter FMT_DATE_TIME = DateTimeFormatter.ofPattern("HH:mm:ss dd/MM/yyyy");
+	public static final DateTimeFormatter FMT_DATE_TIME_VIE = DateTimeFormatter.ofPattern("HH:mm:ss ngày dd/MM/yyyy");
+	public static final NumberFormat FMT_CURRENCY = NumberFormat.getCurrencyInstance(new Locale("vi", "VN"));
+
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> {
-			UIManager.put("Button.focus", ColorUtilities.darken(Color.GRAY));
+			UIManager.put("Button.focus", ColorUtilities.darken(Colors.GRAY_50));
 
 //			DangNhapController.getInstance().showView();
 			TrangChuController.getInstance().showView(new TaiKhoan("vantu", "0", new NhanVien("NV241101")));
